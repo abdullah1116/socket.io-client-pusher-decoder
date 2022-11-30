a77bc5bb212ce01e596bcb6600eb18a20a34ec4b
tmpabdullah <tmp.abdullah1@outlook.com>
2022-12-01 00:09:32 +0500

    0.3.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,40 +1,72 @@
-import { Socket as ISocket } from 'socket.io-client';
-export declare function initPusher(_io: any): typeof Pusher;
+import { io as Iio, Socket as ISocket } from 'socket.io-client';
 declare class Connection {
-    private io;
-    constructor(uri: string, opts?: any);
-    bind(ev: string, listener: typeof IListener): ISocket<import("@socket.io/component-emitter").DefaultEventsMap, import("@socket.io/component-emitter").DefaultEventsMap>;
-    unbind(ev: string, listener: typeof IListener): ISocket<import("@socket.io/component-emitter").DefaultEventsMap, import("@socket.io/component-emitter").DefaultEventsMap>;
+    private socket;
+    constructor(io: typeof Iio, uri: string, opts?: any);
+    bind(eventName: string, listener: typeof IListener): ISocket<import("@socket.io/component-emitter").DefaultEventsMap, import("@socket.io/component-emitter").DefaultEventsMap>;
+    unbind(eventName: string, listener: typeof IListener): ISocket<import("@socket.io/component-emitter").DefaultEventsMap, import("@socket.io/component-emitter").DefaultEventsMap>;
 }
-declare class Pusher {
-    connection: Connection;
-    subscribers: Record<string, Channel>;
-    constructor(url: string, opts?: any);
-    subscribe(ev: any): Channel;
-    unsubscribe(ev: any): this;
+export declare class Pusher {
+    io: Connection;
+    private subscribers;
+    constructor(io: typeof Iio, url: string, opts?: any);
+    /**
+     * Subscribe specified channels.
+     * @param eventName Event name
+     */
+    subscribe(eventName: string): Channel;
+    /**
+     * Un-Subscribe specified channels.
+     * @param eventName Event name
+     */
+    unsubscribe(eventName: string): this;
+    /**
+     * Un-Subscribe all channels.
+     * !!WARNING!!! This will remove all subscribers
+     */
     unsubscribe_all(): this;
-    bind(ev: string, listner: typeof IListener): void;
+    /**
+     * Bind to all events of all channels.
+     * @param eventName Event name
+     * @param listener Listener
+     */
+    bind(eventName: string, listener: typeof IListener): void;
 }
 declare class Channel {
     private pusher;
     private subscriberName;
     eventListeners: Record<string, PusherBinder[]>;
     constructor(pusher: Pusher, subscriberName: string);
-    onEvent(ev: string, data: any): void;
-    bind(ev: any, listner: any): void;
-    unbind(ev: string): this;
+    private onEvent;
+    /**
+     * Bind to specified event of this channel.
+     * @param eventName Event name
+     * @param listener Listener
+     */
+    bind(eventName: string, listener: typeof ISpecifiedListener): void;
+    /**
+     * Un-bind all specified events of this channel.
+     * @param eventName Event name
+     */
+    unbind(eventName: string): this;
+    /**
+     * Un-bind all events of this channel.
+     */
     unbind_all(): this;
+    /**
+     * Un-subscribe this channel.
+     */
     unSubscribe(): void;
 }
 declare class PusherBinder {
     private pusherChannel;
-    private ev;
-    private listner;
-    constructor(pusherChannel: Channel, ev: string, listner: any);
+    private eventName;
+    private listener;
+    constructor(pusherChannel: Channel, eventName: string, listener: typeof ISpecifiedListener);
     emit(data: any): void;
     unbind(): void;
 }
-declare function IListener(...args: any[]): void;
+declare function IListener(eventName: string, data: any): void;
+declare function ISpecifiedListener(data: any): void;
 export interface IPusher extends Pusher {
 }
 export interface IChannel extends Channel {
